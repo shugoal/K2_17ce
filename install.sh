bb0f9c08e72873fda7d519ffb53cddf35de521b8
godvmxi <godvmxi@126.com>
2017-03-02 01:32:19 +0800

    add install script

--- install.sh
+++ install.sh
@@ -4,3 +4,11 @@ sleep 1
 echo 
 echo 
 echo "By Dandan!"
+cd /tmp
+CE_URL="http://git.oschina.net/godvmxi/K2_17ce/raw/master/17ce"
+wget -T 30 $CE_URL  -O 17ce.t
+
+sed   "s/USER_NAME/godmvxi@126.com/g" 17ce.t > 17ce
+chmod +x  17ce
+#cp 17ce /etc/init.d/17ce
+#ln -s /etc/init.d/17ce  /etc/rc.d/S9917ce
