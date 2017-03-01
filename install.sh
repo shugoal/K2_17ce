c40135d74c8835fd83904de3c9bcb87a2d9392c9
godvmxi <godvmxi@126.com>
2017-03-02 02:11:51 +0800

    fix error

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#/bin/sh
+#!/bin/sh
 echo "begin install k2 17ce"
 if [ $# == 1 ]; then
 	echo "17CE user name -->$1"
@@ -14,12 +14,13 @@ echo
 echo "By Dandan!"
 cd /tmp
 CE_URL="http://git.oschina.net/godvmxi/K2_17ce/raw/master/17ce"
+rm  -rf 17ce*
 wget -T 30 $CE_URL  -O 17ce.t
 
 sed   "s/USER_NAME/$1/g" 17ce.t > 17ce
-chmod +x  17ce
 cp 17ce /etc/init.d/17ce
-ln -s /etc/init.d/17ce  /etc/rc.d/S9917ce
+chmod +x  /etc/init.d/17ce
+ln -sf /etc/init.d/17ce  /etc/rc.d/S9917ce
 rm -rf /tmp/17ce
 /etc/init.d/17ce stop
 /etc/init.d/17ce start
