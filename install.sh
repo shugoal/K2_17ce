e5bb9c49557d057c3f43c2af73a2934464713fc2
mjyhj <mjyhj@qq.com>
2017-03-30 21:01:00 +0800

    更新 install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
-CE_URL="http://git.oschina.net/mjyhj/K2_17ce/raw/master/17ce"
 echo "begin install k2 17ce"
+if [ $# == 1 ]; then
+	echo "17CE user name -->$1"
+	echo "if the user name is error ,ctrl +c exit"
+	sleep 3
+else 
+	echo "please use the cmd-->"
+	echo "     ./install.sh   xxx@xxx.com"
+	exit 1
+fi
+echo 
+echo 
+echo "By Dandan!"
 cd /tmp
-killall -9 17ce_v3
+CE_URL="http://git.oschina.net/godvmxi/K2_17ce/raw/master/17ce"
 rm  -rf 17ce*
 wget -T 30 $CE_URL  -O 17ce.t
 
