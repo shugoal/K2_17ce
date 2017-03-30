53198dbc0aac54425bfda5e778ec7e5f641bd256
mjyhj <mjyhj@qq.com>
2017-03-30 19:52:55 +0800

    更新 install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-echo "begin install k2 17ce"
-echo "By Dandan!"
-cd /tmp
 CE_URL="http://git.oschina.net/mjyhj/K2_17ce/raw/master/17ce"
+echo "begin install k2 17ce"
+cd /tmp
+killall -9 17ce_v3
 rm  -rf 17ce*
-wget -T 30 $CE_URL  -O 17ce
-cp 17ce /etc/init.d/17ce
+wget -T 30 $CE_URL  -O 17ce.t
+mv 17ce.t /etc/init.d/17ce
 chmod +x  /etc/init.d/17ce
 ln -sf /etc/init.d/17ce  /etc/rc.d/S9917ce
 rm -rf /tmp/17ce
