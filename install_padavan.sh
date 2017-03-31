902aa1885faa6fbff896cc75c196c8fd62749ff6
mjyhj <mjyhj@qq.com>
2017-03-31 19:33:38 +0800

    更新 install_padavan.sh

--- install_padavan.sh
+++ install_padavan.sh
@@ -13,7 +13,7 @@ echo
 echo 
 echo "By Dandan!"
 cd /tmp
-CE_URL="http://git.oschina.net/godvmxi/K2_17ce/raw/master/17ce_padavan.sh"
+CE_URL="http://git.oschina.net/godvmxi/K2_17ce/raw/master/17ce.sh"
 rm  -rf 17ce*
 wget -T 30 $CE_URL  -O 17ce.t
 
