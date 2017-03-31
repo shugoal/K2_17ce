2bfc35d199f7f617e9bd4dcd5927f6036d152ad7
mjyhj <mjyhj@qq.com>
2017-03-31 21:15:15 +0800

    Upload install_padavan.sh

--- install_padavan.sh
+++ install_padavan.sh
@@ -11,15 +11,14 @@ else
 fi
 echo 
 echo 
-echo "By Dandan!"
-cd /tmp
-CE_URL="http://git.oschina.net/godvmxi/K2_17ce/raw/master/17ce.sh"
+echo "By Dandan!And mjyhj update!"
+rm -rf /etc/storage/17ce
+rm -rf /tmp/17ce
 rm  -rf 17ce*
-wget -T 30 $CE_URL  -O 17ce.t
-
+cd /tmp
+wget -O 17ce.t http://git.oschina.net/mjyhj/K2_17ce/raw/master/17ce.sh
 sed   "s/USER_NAME/$1/g" 17ce.t > 17ce_padavan.sh
+mkdir /etc/storage/17ce
 cp 17ce_padavan.sh /etc/storage/17ce/17ce_padavan.sh
 chmod +x  /etc/storage/17ce/17ce_padavan.sh
-rm -rf /tmp/17ce
-/etc/storage/17ce/17ce_padavan.sh stop
-/etc/storage/17ce/17ce_padavan.sh start
+/etc/storage/17ce/17ce_padavan.sh
