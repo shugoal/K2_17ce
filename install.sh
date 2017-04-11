08e3d90d26d6fa99583395ffc14d7813c65c8f86
mjyhj <mjyhj@qq.com>
2017-04-11 23:37:14 +0800

    Upload install.sh

--- install.sh
+++ install.sh
@@ -11,16 +11,20 @@ else
 fi
 echo 
 echo 
-echo "By Dandan£¡"
-echo "mjyhj update£¡And use myself£¡"
+echo "By Dandan!"
+echo "mjyhj update!And use myself!"
 rm -rf /etc/storage/17ce
 rm -rf /tmp/17ce
 rm  -rf 17ce*
 cd /tmp
-wget -O 17ce.t http://git.oschina.net/mjyhj/K2_17ce/raw/master/17ce_padavan.sh
-sed   "s/USER_NAME/$1/g" 17ce.t > 17ce_padavan.sh
+wget -O 17ce.t http://git.oschina.net/mjyhj/K2_17ce/raw/master/17ce.sh
+sed   "s/USER_NAME/$1/g" 17ce.t > 17ce.sh
 mkdir /etc/storage/17ce
-cp 17ce_padavan.sh /etc/storage/17ce/17ce_padavan.sh
-chmod +x  /etc/storage/17ce/17ce_padavan.sh
-echo "/etc/storage/17ce/17ce_padavan.sh">>/etc/storage/post_wan_script.sh
-/etc/storage/17ce/17ce_padavan.sh
+cp 17ce.sh /etc/storage/17ce/17ce.sh
+chmod +x  /etc/storage/17ce/17ce.sh
+if grep -wq "17ce.sh" /etc/storage/post_wan_script.sh; then
+  /etc/storage/17ce/17ce.sh
+else
+  echo "/etc/storage/17ce/17ce.sh">>/etc/storage/post_wan_script.sh
+  /etc/storage/17ce/17ce.sh
+fi
